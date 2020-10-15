1c9c7c0da282369f754cd45dab0dec7e7aaf672d
Alan luu <luumanhquan.91@gmail.com>
2020-10-15 14:17:07 +0700

    branch base

--- server.js
+++ server.js
@@ -1,18 +1,6 @@
 const koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const session = require('koa-session');
-const KoaConvert = require('koa-convert');
-const mount = require('koa-mount');
-const graphqlHTTP = require('koa-graphql');
-
-const { schemaAdmin } = require('./graphql/admin/index');
-const { schemaWeb } = require('./graphql/web/index');
-
-const { contextAdmin } = require('./auth/admin');
-const { contextShop } = require('./auth/web');
-
-const adminRouter = require('./api/admin');
-const webRouter = require('./api/web');
 
 const app = new koa();
 
@@ -32,47 +20,6 @@ const CONFIG = {
 };
 
 app.use(session(CONFIG, app));
-
-app
-    .use(adminRouter.login.routes())
-    .use(adminRouter.signup.routes())
-    .use(adminRouter.collection.routes())
-    .use(adminRouter.customer.routes())
-    .use(adminRouter.product.routes());
-
-app
-    .use(webRouter.login.routes())
-    .use(webRouter.signup.routes())
-    .use(webRouter.collection.routes())
-    .use(webRouter.product.routes());
-
-app.use(
-    mount(
-        '/graphql/admin',
-        graphqlHTTP(async(ctx) => ({
-            schema: schemaAdmin,
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: 'application/json',
-                token: contextAdmin(ctx),
-            },
-        }))
-    )
-);
-
-app.use(
-    mount(
-        '/graphql',
-        graphqlHTTP(async(ctx) => ({
-            schema: schemaWeb,
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: 'application/json',
-                token: contextShop(ctx),
-            },
-        }))
-    )
-);
 
 const port = process.env.PORT || 9000;
 app.listen(port, () => {
