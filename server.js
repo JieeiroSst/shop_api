94b37ad5b544dbdf07420881280fe557d8a2af92
Alan luu <luumanhquan.91@gmail.com>
2020-10-15 22:04:06 +0700

    add file

--- server.js
+++ server.js
@@ -1,25 +1,18 @@
 const koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const passport = require('passport');
 const session = require('koa-session');
+
+const loginRouter = require('./router/login');
 
 const app = new koa();
 
 app.use(bodyParser());
+app.use(session(app));
+app.use(passport.initialize());
+app.use(passport.session());
 
-const CONFIG = {
-    key: 'koa.sess',
-    maxAge: 86400000,
-    autoCommit: true,
-    overwrite: true,
-    httpOnly: true,
-    signed: true,
-    rolling: false,
-    renew: false,
-    secure: true,
-    sameSite: null,
-};
-
-app.use(session(CONFIG, app));
+app.use(loginRouter.routes());
 
 const port = process.env.PORT || 9000;
 app.listen(port, () => {
